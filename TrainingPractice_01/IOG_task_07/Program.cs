4b8ec8fd2df0ec34d5be60f9a053d10f1c9e9624
olner <izmailov2003@yandex.ru>
2022-02-22 19:05:31 +0300

    1-7

---
+++ TrainingPractice_01/IOG_task_07/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOG_task_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа перемешивнаия массива");
+            Console.Write("Введите кол-во элементов одномерного массива ");
+            int[] mas = new int[int.Parse(Console.ReadLine())];
+            var rnd = new Random();
+            for (var i = 0;i < mas.Length; i++)
+            {
+                mas[i] = rnd.Next(1, 100);
+            }
+            Console.WriteLine("Массив до перемешивания");
+            for (var i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine(mas[i]);
+            }
+            var shuffledMassive = Shuffle(mas);
+            Console.WriteLine("Массив после перемешивания");
+            for (var i = 0; i < shuffledMassive.Length; i++)
+            {
+                Console.WriteLine(shuffledMassive[i]);
+            }
+            Console.ReadLine();
+        }
+        static int[] Shuffle(int[] massive)
+        {
+            var mas = massive;
+            Random rnd = new Random();
+
+            for (int i = mas.Length - 1; i >= 1; i--)
+            {
+                int j = rnd.Next(i + 1);
+
+                int tmp = mas[j];
+                mas[j] = mas[i];
+                mas[i] = tmp;
+            }
+            return mas;
+        }
+    }
+}
